2fa02d6e4c7c9a3bcf3d8fca488c84aeb1cf2b6d
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2022-06-04 23:37:47 +0800

    Update ProjectMotionCard.tsx

--- components/ProjectMotionCard.tsx
+++ components/ProjectMotionCard.tsx
@@ -41,8 +41,6 @@ export default function ProjectMotionCard({ id, backgroundImage, type, projectNa
           }} >
             <img src={backgroundImage}
               alt="kuchen"
-              layout="fill"
-              objectFit="cover"
             />
             <div
               className='title'
