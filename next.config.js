c8ef66a3d8173374d0978675aa72b7323fa0259f
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2022-06-05 00:03:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     domains: [
+      'res.cloudinary.com',
       'images.ctfassets.net',
       'www.kuchen.com.hk',
       'kuchen.vercel.app',
