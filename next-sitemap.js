ac23f1ea64439662f03e8beaeebfb3291cde0bed
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2023-07-23 13:38:03 +0800

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,17 +1,12 @@
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL,
     generateRobotsTxt: true,
-    exclude: excludedPaths + ["/[sitemap]"],
     robotsTxtOptions: {
       policies: [
         {
           userAgent: "*",
           allow: "/",
-        },
-        {
-          userAgent: "*",
-          disallow: excludedPaths,
-        },
+        }
       ],
     },
   }
